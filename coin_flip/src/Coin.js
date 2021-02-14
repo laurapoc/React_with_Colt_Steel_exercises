82ddcb3d4993fdad6a09346f12fb119b28ba438a
laurapoc <laura.pociuniene@gmail.com>
2021-02-14 16:03:13 +0200

    coin flip solution

--- coin_flip/src/Coin.js
+++ coin_flip/src/Coin.js
@@ -3,12 +3,6 @@ import "./Coin.css";
 
 export default class Coin extends Component {
   render() {
-    return (
-      <img
-        className="Coin"
-        src={this.props.coin}
-        alt=""
-      />
-    );
+    return <img className="Coin" src={this.props.coin} alt="" />;
   }
 }
--- coin_flip/src/CoinFlipContainer.js
+++ coin_flip/src/CoinFlipContainer.js
@@ -2,20 +2,51 @@ import React, { Component } from "react";
 import Coin from "./Coin";
 
 export default class CoinFlipContainer extends Component {
+  static defaultProps = {
+    coinFaces: [
+      { face: "https://tinyurl.com/react-coin-heads-jpg" },
+      { face: "http://www.pcgscoinfacts.com/UserImages/71009269r.jpg" },
+    ],
+  };
+
   state = {
-    coin: "",
-    // head: "https://tinyurl.com/react-coin-heads-jpg",
-    // tail: "https://tinyurl.com/react-coin-tails-jpg",
+    coin: null,
+    numOfHeads: 0,
+    numOfTails: 0,
+    numOfClicks: 0,
   };
 
   showCoinHandler = () => {
-    this.setState({ coin: "https://tinyurl.com/react-coin-heads-jpg" });
+    let head = this.setState((st) => {
+      return {
+        coin: this.props.coinFaces[0].face,
+        numOfHeads: st.numOfHeads + 1,
+      };
+    });
+    return head;
+  };
+
+  switchCoinHandler = () => {
+    let tail = this.setState((st) => {
+      return {
+        coin: this.props.coinFaces[1].face,
+        numOfTails: st.numOfTails + 1,
+      };
+    });
+    return tail;
   };
 
   buttonClickHandler = () => {
-   this.showCoinHandler()
-   
- 
+    let coin =
+      this.state.coin === null || this.state.coin === this.props.coinFaces[1].face
+        ? this.showCoinHandler()
+        : this.switchCoinHandler();
+    this.setState((st) => {
+      return {
+        numOfClicks: st.numOfClicks + 1,
+      };
+    });
+    return coin;
   };
 
   render() {
@@ -26,6 +57,9 @@ export default class CoinFlipContainer extends Component {
           <Coin coin={this.state.coin} />
         </div>
         <button onClick={this.buttonClickHandler}>Flip Me!</button>
+        <h3>
+          Out of {this.state.numOfClicks} flips there have been {this.state.numOfHeads} heads and {this.state.numOfTails} tails.
+        </h3>
       </div>
     );
   }
