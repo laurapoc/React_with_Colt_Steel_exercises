1b8ca975ce37b278fe5e8ddccf23b57a7345c7a2
laurapoc <laura.pociuniene@gmail.com>
2021-03-22 17:11:19 +0200

    add button to get more chips

--- vending-machine/src/Chips.js
+++ vending-machine/src/Chips.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./Chips.css";
 
 export default class Chips extends Component {
+  static defaultProps = {
+    imageSource: "https://images-na.ssl-images-amazon.com/images/I/81vJyb43URL._SL1500_.jpg",
+    maxNum: 6,
+  };
+
+  state = {
+    images: [],
+  };
+
+  handleButtonClick = () => {
+    if (this.state.images.length < this.props.maxNum) {
+      this.setState((prevState) => ({
+        images: [...prevState.images, this.props.imageSource],
+      }));
+    } else {
+      alert("You can not add more chips");
+    }
+
+    console.log(this.state.images);
+  };
+
   render() {
+    let imageList = this.state.images.map((img) => <img src={img} alt="Chips Lays" />);
     return (
-      <div>
-        <h2>Chips component</h2>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/81vJyb43URL._SL1500_.jpg" alt="Chips Lays" />
+      <div className="Chips">
+        <h1>CHIPS COMPONENT</h1>
+        <button onClick={this.handleButtonClick}>Get New Chips Pack!</button>
+        <div className="chips-images">{imageList}</div>
         <div>
           <NavLink to="/">GO TO HOMEPAGE</NavLink>
         </div>
